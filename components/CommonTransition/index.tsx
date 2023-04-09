2a1dd961cb88e707ea9f07dcb8c2fe0c31e1f7e0
liuming <15926724726@163.com>
2023-04-09 17:40:35 +0800

    fix: CommonTransition

--- components/CommonTransition/index.tsx
+++ components/CommonTransition/index.tsx
@@ -3,19 +3,20 @@ import React, { FC, useState } from 'react';
 import { ITransitionProps } from './type';
 
 const CommonTransition: React.FC<ITransitionProps> = (props) => {
-  const { classNames, children, animation, ...restProps } = props;
+  // const { classNames, children, animation, ...restProps } = props;
 
-  return (
-    <CSSTransition
-      className={classNames ? classNames : animation}
-      {...restProps}
-    >
-      {children}
-    </CSSTransition>
-  )
+  // return (
+  //   <CSSTransition
+  //     className={classNames ? classNames : animation}
+  //     {...restProps}
+  //   >
+  //     {children}
+  //   </CSSTransition>
+  // )
+  return <></>
 }
-CommonTransition.defaultProps = {
-  unmountOnExit: true,
-  appear: true,
-}
+// CommonTransition.defaultProps = {
+//   unmountOnExit: true,
+//   appear: true,
+// }
 export default CommonTransition;
